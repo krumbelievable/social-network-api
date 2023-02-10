0b8a81570c9cdc30e8cd8c21727166ca06e78a60
Krumbelievable <krumbach.will@gmail.com>
2023-02-10 17:08:09 -0600

    added comments and fixed some code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
-//exporting our API routes.
 
 router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
 
+//exports all of our routes
 module.exports = router;
